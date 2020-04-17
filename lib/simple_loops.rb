b56a36f072ef08af937c46212a55239db3689bb3
energetic-commit-5967 <jmcelroy@plexustech.net>
2020-04-17 14:20:38 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,3 +16,10 @@ def loop_message_n_times(string, integer)
     counter += 1 
   end 
 end
+
+def output_array(array)
+  counter = 0 
+  while counter < array.length do
+    puts array[counter]
+  end
+end
